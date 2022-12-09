a6a503250ede78ae0ed303f8fcc110d02700469f
Kristian Kosev <kosev.sou@gmail.com>
2022-12-09 02:50:27 +0200

    Update-2 Crouch, Sprint
    Slope is very messy I'll need to figure that one out

--- Start/Assets/Scripts/PlayerMovement.cs
+++ Start/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
+    public float crouchSpeed;
+
+    public float crouchYscale;
+    private float startYscale;
 
     public float groundSlide;
 
@@ -15,6 +21,8 @@ public class PlayerMovement : MonoBehaviour
     bool readyToJump = true ;
 
     public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
 
     public float playerHeight;
     public LayerMask groundDetect;
@@ -25,14 +33,30 @@ public class PlayerMovement : MonoBehaviour
     float horizontalInput;
     float verticalInput;
 
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
+
+
     Vector3 moveDirection;
 
     Rigidbody rb;
+
+    public MovementState state;
+
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        crouching,
+        air,
+    }
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+
+        startYscale = transform.localScale.y;
     }
 
     private void Update()
@@ -42,6 +66,7 @@ public class PlayerMovement : MonoBehaviour
 
         MyInput();
         SpeedControl();
+        StateHandler();
 
         if (grounded)
             rb.drag = groundSlide;
@@ -66,17 +91,66 @@ public class PlayerMovement : MonoBehaviour
             Jump();
             Invoke(nameof(ResetJump), jumpCooldown);
         }
+        if (Input.GetKeyDown(crouchKey))
+        {
 
+            transform.localScale = new Vector3(transform.localScale.x, crouchYscale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+
+        }
+
+        if (Input.GetKeyUp(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYscale, transform.localScale.z);
+
+        }
+
+    }
+
+    private void StateHandler()
+    {
+        if (Input.GetKey(crouchKey))
+        {
+            state = MovementState.crouching;
+            moveSpeed = crouchSpeed;
+        }
+        if(grounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+        }
+       else if (grounded)
+        {
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+}
+
+        else
+        {
+            state = MovementState.air;
+        }
     }
 
     private void MovePlayer()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if (OnSlope())
+        {
+            rb.AddForce(GetSlopeDir() * moveSpeed * 25f, ForceMode.Force);
+
+            if (rb.velocity.y > 0)
+                rb.AddForce(Vector3.down * 50f, ForceMode.Force);
+        }
+
         if(grounded)
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
 
         else if(!grounded)
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+
+        rb.useGravity = !OnSlope();
+
 
 
 
@@ -101,6 +175,20 @@ public class PlayerMovement : MonoBehaviour
     public void ResetJump()
     {
         readyToJump = true;
+    }
+
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle!= 0;
+        }
+        return false;
+    }
+    private Vector3 GetSlopeDir()
+    {
+        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
     }
 }
 
